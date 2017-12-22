785c98b4dfa035560e8a2f31d300ec88a4a83fbc
elslander-cedric <cedric.elslander@diebold.com>
2017-12-22 14:31:20 +0100

    bundle dist dir

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
-var gulp = require('gulp');
-var gzip = require('gulp-gzip');
+const gulp = require('gulp');
+const tar = require('gulp-tar');
+const gzip = require('gulp-gzip');
 
 gulp.task('compress', function() {
-  gulp.src(['./dist/*.js'])
+  gulp.src(['./dist/**'])
+    .pipe(tar('bundle.tar'))
     .pipe(gzip())
-    .pipe(gulp.dest('./dist'));
+    .pipe(gulp.dest('./dist/'));
 });
